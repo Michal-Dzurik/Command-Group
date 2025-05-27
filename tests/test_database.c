40bb543bf84b2d12cf694aa3342a96fdf09f5491
Michal Dzurík <misko7104@gmail.com>
2025-05-27 08:47:13 +0200

    #9 Test refactor

--- tests/test_database.c
+++ tests/test_database.c
@@ -2,9 +2,22 @@
 
 #include <criterion/criterion.h>
 
+#define DATABASE_NAME "db_test.db"
+
+#define GROUP_NAME "group_name"
+#define GROUP_NAME_1 "example_group1"
+#define GROUP_NAME_2 "example_group2"
+#define NON_EXISTING_NAME "non-existing"
+
+#define COMMAND_NAME "example_command"
+#define COMMAND_NAME_1 "example_command1"
+#define COMMAND_NAME_2 "example_command2"
+
+#define LS "ls"
+
 void prepare(){
     db_init("");
-    db_set_test_name("db_test.db");
+    db_set_test_name(DATABASE_NAME);
 }
 
 void destroy(){
@@ -21,7 +34,7 @@ Test(database, create_group_dry) {
 }
 
 Test(database, create_group) {
-    int result = db_add_group("example_group");
+    int result = db_add_group(GROUP_NAME);
 
     cr_assert_neq(result,0);
 }
@@ -33,12 +46,12 @@ Test(database, remove_group_dry) {
 }
 
 Test(database, remove_group) {
-    db_add_group("example_group");
+    db_add_group(GROUP_NAME);
 
-    int result = db_remove_group("example_group");
+    int result = db_remove_group(GROUP_NAME);
     cr_assert_eq(result,1);
 
-    result = db_remove_group("non-existing");
+    result = db_remove_group(NON_EXISTING_NAME);
     cr_assert_eq(result,1);
 }
 
@@ -50,12 +63,12 @@ Test(database, list_group_dry) {
 }
 
 Test(database, list_group) {
-    db_add_group("example_group");
-    db_add_group("example_group2");
+    db_add_group(GROUP_NAME);
+    db_add_group(GROUP_NAME_1);
 
     char *expected[2] = {
-        "example_group",
-        "example_group2"
+        GROUP_NAME,
+        GROUP_NAME_1
     };
 
     int count = 0;
@@ -77,12 +90,12 @@ Test(database, create_command_dry) {
 }
 
 Test(database, create_command) {
-    db_add_group("example_group");
+    db_add_group(GROUP_NAME);
 
-    int result = db_add_command("example_group","example_command","ls");
+    int result = db_add_command(GROUP_NAME,COMMAND_NAME,LS);
     cr_assert_eq(result,1);
 
-    result = db_add_command("non-existing","example_command","ls");
+    result = db_add_command(NON_EXISTING_NAME,COMMAND_NAME,LS);
 
     cr_assert_eq(result,1);   
 }
@@ -94,17 +107,17 @@ Test(database, get_command_dry) {
 }
 
 Test(database, get_command) {
-    db_add_command("example_group","example_command","ls");
+    db_add_command(GROUP_NAME,COMMAND_NAME,LS);
 
-    char* result = db_get_command("example_group","example_command");
-    cr_assert_str_eq(result,"ls");
+    char* result = db_get_command(GROUP_NAME,COMMAND_NAME);
+    cr_assert_str_eq(result,LS);
     free(result);
 
-    result = db_get_command("example_group","f");
+    result = db_get_command(GROUP_NAME,COMMAND_NAME_1);
     cr_assert_eq(result,NULL);
     free(result);
 
-    result = db_get_command("f","f");
+    result = db_get_command(GROUP_NAME_1,COMMAND_NAME_1);
     cr_assert_eq(result,NULL);
     free(result);
 }
@@ -116,32 +129,32 @@ Test(database, remove_command_dry) {
 }
 
 Test(database, remove_command) {
-    db_add_group("example_group");
+    db_add_group(GROUP_NAME);
 
-    int result = db_add_command("example_group","example_command","ls");
+    int result = db_add_command(GROUP_NAME,COMMAND_NAME,LS);
 
-    result = db_remove_command("example_group","example_command");
+    result = db_remove_command(GROUP_NAME,COMMAND_NAME);
     cr_assert_eq(result,1);
 
-    result = db_remove_command("non-existing","example_command");
+    result = db_remove_command(NON_EXISTING_NAME,COMMAND_NAME);
     cr_assert_eq(result,1);
 
-    result = db_remove_command("non-existing","non-existing");
+    result = db_remove_command(NON_EXISTING_NAME,NON_EXISTING_NAME);
     cr_assert_eq(result,1);
 }
 
 // Realtions
 Test(database, remove_group_and_commands) {
-    db_add_command("example_group","example_command1","ls");
-    db_add_command("example_group","example_command2","ls");
-    db_add_command("example_group","example_command3","ls");
+    db_add_command(GROUP_NAME,COMMAND_NAME,LS);
+    db_add_command(GROUP_NAME,COMMAND_NAME_1,LS);
+    db_add_command(GROUP_NAME,COMMAND_NAME_2,LS);
 
-    int result = db_remove_group("example_group");
+    int result = db_remove_group(GROUP_NAME);
     cr_assert_eq(result,1);
 
-    cr_assert_eq(db_get_command("example_group","example_command1"),NULL);
-    cr_assert_eq(db_get_command("example_group","example_command2"),NULL);
-    cr_assert_eq(db_get_command("example_group","example_command3"),NULL);
+    cr_assert_eq(db_get_command(GROUP_NAME,COMMAND_NAME),NULL);
+    cr_assert_eq(db_get_command(GROUP_NAME,COMMAND_NAME_1),NULL);
+    cr_assert_eq(db_get_command(GROUP_NAME,COMMAND_NAME_2),NULL);
 }
 
 Test(database, list_commands_in_group_dry) {
@@ -149,25 +162,25 @@ Test(database, list_commands_in_group_dry) {
     char **result = db_get_command_list(NULL,&count);
     cr_assert_eq(result, NULL);
 
-    db_add_group("example_group");
-    result = db_get_command_list("example_group",&count);
+    db_add_group(GROUP_NAME);
+    result = db_get_command_list(GROUP_NAME,&count);
     cr_assert_eq(result, NULL);
 }
 
 Test(database, list_commands_in_group) {
-    db_add_group("example_group");
-    db_add_command("example_group","example_command1","ls");
-    db_add_command("example_group","example_command2","ls");
-    db_add_command("example_group","example_command3","ls");
+    db_add_group(GROUP_NAME);
+    db_add_command(GROUP_NAME,COMMAND_NAME,LS);
+    db_add_command(GROUP_NAME,COMMAND_NAME_1,LS);
+    db_add_command(GROUP_NAME,COMMAND_NAME_2,LS);
 
     char *expected[3] = {
-        "example_command1",
-        "example_command2",
-        "example_command3"
+        COMMAND_NAME,
+        COMMAND_NAME_1,
+        COMMAND_NAME_2
     };
 
     int count = 0;
-    char **result = db_get_command_list("example_group",&count);
+    char **result = db_get_command_list(GROUP_NAME,&count);
     cr_assert_eq(count, 3);
     
     for (int i = 0; i < count; i++){
