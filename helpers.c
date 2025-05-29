4604988d2f9614acd130dc3fa83ce6475d11050b
Michal Dzurík <misko7104@gmail.com>
2025-05-29 19:09:03 +0200

    Test helpers fix

--- helpers.c
+++ helpers.c
@@ -9,7 +9,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// PROGRAM
 char* to_lower(char* s) {
     if (!s) return NULL;
 
@@ -17,40 +16,4 @@ char* to_lower(char* s) {
         *p = (char)tolower((unsigned char)*p);
     }
     return s;
-}
-
-// TESTS
-FILE* switch_stderr_for_temp() {
-    FILE *file = fopen(TEMP_FILE, "w+");
-    if (file == NULL) {
-        cr_assert_fail("Failed to create temporary file: %s", TEMP_FILE);
-        return NULL;
-    }
-
-    fclose(file);
-
-    FILE *original_stderr = stderr;
-    stderr = file;
-    return original_stderr;
-}
-
-void switch_temp_for_stderr(FILE *original_stderr){
-    if (original_stderr == NULL) return;
-
-    stderr = original_stderr;
-}
-
-int is_file_empty(char *file_name){
-    FILE *file = fopen(file_name,"w");
-    if (file == NULL) {
-        cr_assert_fail("Failed to read file: %s", file_name);
-        return false;
-    }
-
-    rewind(file);
-    char buffer[1024] = {0};
-    size_t bytes_read = fread(buffer, 1, sizeof(buffer) - 1, file);
-    fclose(file);
-
-    return bytes_read == 0;
 }
